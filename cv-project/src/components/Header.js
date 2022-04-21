b11cfea642a3484daec72e0db1b60f0403ecb72d
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-04-21 23:56:43 +0300

    Setup tailwind & create Header.js

---
+++ cv-project/src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className='container h-16 flex justify-center items-center bg-sky-500'>
+          <h2 className="text-sky-800 font-bold">My CV</h2>
+      </div>
+    )
+  }
+}
